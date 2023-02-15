805f21c45cb7804cc39f1b1e010cccce06aba38e
Alejandra <alejamantillac@gmail.com>
2023-02-15 09:17:15 -0500

    First commit

---
+++ src/DobleEnlace.java
@@ -0,0 +1,77 @@
+public class DobleEnlace {
+    private Node head;
+    private Node tail;
+
+    public void addNode(Node node){
+        if(head==null){
+            head = node;
+            tail = node;
+        }else{
+            tail.setNext(node);
+            node.setPrevious(tail);
+            tail = node;
+        }
+    }
+
+    public void addFirst(Node node){
+        if(head==null){
+            head = node;
+            tail = node;
+        }else{
+            node.setNext(head);
+            head.setPrevious(node);
+            head = node;
+        }
+    }
+
+    //Activacion
+    public void print(){
+        print(head);
+    }
+
+    //Recursivo
+    private void print(Node current){
+        if(current == null){
+            return;
+        }
+        System.out.println(current.getName());
+        print(current.getNext());
+    }
+
+    //Eliminacion
+    public void delete(String goal){
+        delete(head, goal);
+    }
+    private void delete(Node current, String goal){
+        if(current == null){
+            return;
+        }
+        if(current.getName().equals(goal)){
+            if(current == head) {
+                Node next = current.getNext();
+                next.setPrevious(null);
+                head = next;
+                return;
+            }
+
+            if(current == tail) {
+                Node prev = head.getPrevious();
+                prev.setNext(null);
+                tail = prev;
+                return;
+            }
+            //Eliminar
+            Node prev = current.getPrevious();
+            Node next = current.getNext();
+            prev.setNext(next);
+            next.setPrevious(prev);
+            return;
+        }
+        delete(current.getNext(), goal);
+
+    }
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        DobleEnlace lista = new DobleEnlace();
+        lista.addNode(
+                new Node("A")
+        );
+        lista.addNode(
+                new Node("B")
+        );
+        lista.addNode(
+                new Node("C")
+        );
+        lista.addNode(
+                new Node("D")
+        );
+        lista.addFirst(
+                new Node("Z")
+        );
+        lista.print();
+        lista.delete("Z");
+        System.out.println("****");
+        lista.print();
+
+    }
+}
